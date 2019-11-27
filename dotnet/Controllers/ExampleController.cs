5ebdad5de487521dacd1ac85e6933e420bb7eca0
Renato Carvalho <renatoluizcarvalho@gmail.com>
2019-11-27 16:45:36 -0300

    Commit project with docker support.

---
+++ dotnet/Controllers/ExampleController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExampleController : ControllerBase
+    {
+        [HttpGet]
+        public string[] Get()
+        {
+            return new string[] { "Hello World" };
+        }
+    }
+}
